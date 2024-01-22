514bb072e15633acec52147218650dd8de4a4bca
avasilenko <snakevil@mail.ru>
2024-01-22 13:23:13 +0300

    task1 modify

--- task1/Program.cs
+++ task1/Program.cs
@@ -18,7 +18,7 @@ class Program
                 while (number > 0)  
                 {
                     sum += number % 10; 
-                number /= 10;
+                    number /= 10;
                 }
                 if (sum % 2 == 0) 
                 {
