f7ebf0b4e737077bd198fe83d0fe88cc2ab1f758
avasilenko <snakevil@mail.ru>
2024-01-22 13:21:58 +0300

    task3 DONE

---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 3, 5, 6, 7, 8 };
+        int temp;
+
+        foreach (int number in numbers)
+        {
+            Console.Write($" {number}");
+        }
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+
+            temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - 1 - i];
+            numbers[numbers.Length - 1 - i] = temp;
+        }
+        Console.Write("\n");
+        foreach (int number in numbers)
+        {
+
+            Console.Write($" {number}");
+        }
+    }
+}
