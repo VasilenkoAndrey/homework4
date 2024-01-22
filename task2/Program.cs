bc01add3d89aa78d19546ce8ada83305953ad8e3
avasilenko <snakevil@mail.ru>
2024-01-22 12:58:34 +0300

    Task2 done

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[10];
+        Random random = new Random();
+        int evenCount = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+            Console.Write(numbers[i] + " ");
+            if (numbers[i] % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+        Console.WriteLine(" ");
+        Console.WriteLine($"четных чисел: {evenCount}");
+    }
+}
