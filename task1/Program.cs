a923d9e41ad7c7a1c5d638bd463128880cd4868e
avasilenko <snakevil@mail.ru>
2024-01-22 12:46:51 +0300

    Task1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        while (true) 
+        {
+            Console.Write("Введите число или символ [q] для выхода из программы ");
+            string input = Console.ReadLine();
+            if (input == "q") 
+            {
+                break;
+            }
+            int number;
+            if (int.TryParse(input, out number)) 
+            {
+                int sum = 0;
+                while (number > 0)  
+                {
+                    sum += number % 10; 
+                number /= 10;
+                }
+                if (sum % 2 == 0) 
+                {
+                    Console.WriteLine("СУММА ЧЕТНАЯ");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("ОШИБКА: введите 'q' или число .");
+            }
+        }
+    }
+}
